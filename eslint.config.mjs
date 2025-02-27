b340aa0155a7c6f2b33bbfc55f2f43b6d19979b1
Quercaesii <somsit.thanapat@gmail.com>
2025-02-27 14:40:16 +0700

    Add Dockerfile

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ const eslintConfig = [
     extends: ['next'],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-empty-interface-type": "true",
+      "@typescript-eslint/no-empty-interface": "true",
     },
   }),
 ];
